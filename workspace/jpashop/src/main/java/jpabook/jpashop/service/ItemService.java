d57da772941fd474f776f4ca2bde4fdbb9d5036b
YongWookLEE <lsd1008@naver.com>
2022-09-18 21:51:39 +0900

    09-18

--- workspace/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ workspace/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +16,26 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    /**
+     * 병합 : 병합시 모든 속성 변경, 값이 없으면 null로 업데이트
+     */
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    /**
+     * 변경 감지 : 원하는 속성만 선택해서 변경 가능
+     *            merge를 할 경우 Null값으로 update 될 위험이 있어 변경 감지 사용을 권장
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+          // 찾아오면 영속상태 -> flush를 날려 영속상태 변경점을 감지해 업데이트
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+
     }
 
     public List<Item> findItem(){
